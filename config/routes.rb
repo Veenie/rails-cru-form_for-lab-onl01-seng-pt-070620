ad630c1a5940e34a06d07f5a688c101b83be975f
Raveen K Sharma <raveen08@gmail.com>
2020-11-11 22:37:13 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/index'
+
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/edit'
+
+  get 'genres/update'
+
+  get 'genres/index'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
+  get 'artists/index'
+  
+  get 'artists/:id', to: 'artists#show'
+  get 'songs/:id', to: 'songs#show'
+  get 'genres/:id', to: 'genres#show'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
